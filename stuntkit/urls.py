1cbdee534acab142bfeab79ff7dc19ceea56a724
mauroindigne <mauroindigne@gmail.com>
2020-08-19 13:51:12 +0000

    working stuntkit_details view, starting layout of infomation + form

--- stuntkit/urls.py
+++ stuntkit/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.view_stuntkit, name='view_stuntkit')
+    path('', views.view_stuntkit, name='view_stuntkit'),
+    path('stuntkit', views.view_stuntkit_detail, name='view_stuntkit_detail')
 ]
--- stuntkit/views.py
+++ stuntkit/views.py
@@ -5,3 +5,7 @@ from django.shortcuts import render
 def view_stuntkit(request):
     """ A view to return the stuntkit page """
     return render(request, 'stuntkit/stuntkit.html')
+
+def view_stuntkit_detail(request):
+    """ A view to return the stuntkit page """
+    return render(request, 'stuntkit/stuntkit_detail.html')
