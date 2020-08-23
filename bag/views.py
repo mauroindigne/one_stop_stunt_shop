4da1d9b3006f2d696b0814faec75fd2ba023c25f
mauroindigne <mauroindigne@gmail.com>
2020-08-23 14:46:24 +0000

    added remove option in bag

--- bag/views.py
+++ bag/views.py
@@ -7,11 +7,25 @@ def view_bag(request):
 
     return render(request, 'bag/bag.html')
 
+
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
+    color = None
+    if 'product_color' in request.POST:
+        color = request.POST['product_color']
+    bag = request.session.get('bag', {})
+
+    if color:
+        if item_id in list(bag.keys()):
+            if color in bag[item_id]['items_by_color'].keys():
+                bag[item_id]['items_by_color'][color] += quantity
+            else:
+                bag[item_id]['items_by_color'][color] = quantity
+        else:
+            bag[item_id] = {'items_by_color': {color: quantity}}
     bag = request.session.get('bag', {})
 
     if item_id in list(bag.keys()):
