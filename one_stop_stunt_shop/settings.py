a4ce4efcc549520601ff2a80c0a7f6b4dcd07bc8
mauroindigne <mauroindigne@gmail.com>
2020-08-27 10:44:11 +0000

    added checkout template and functionality

--- one_stop_stunt_shop/settings.py
+++ one_stop_stunt_shop/settings.py
@@ -47,6 +47,9 @@ INSTALLED_APPS = [
     'stuntkit',
     'bag',
     'checkout',
+
+    # Others
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -60,6 +63,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'one_stop_stunt_shop.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -77,6 +82,10 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
                 'bag.contexts.bag_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
