efb2e2a2eadb2a5d1a1a4e9960bdf920d0767027
mauroindigne <mauroindigne@gmail.com>
2020-08-27 12:12:34 +0000

    added stripe card element

--- checkout/views.py
+++ checkout/views.py
@@ -15,6 +15,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51HA9qfHlDvQ76wYUpIH7qW0wTZaA67L9gLs0AOVGmLNfTGiev8Xw0w9juSfNCYrEcC8WiJ1OPl7sWFBcWWRpgVjO00jxrilbeS',
+        'client_secret': 'test client secret'
     }
 
     return render(request, template, context)
