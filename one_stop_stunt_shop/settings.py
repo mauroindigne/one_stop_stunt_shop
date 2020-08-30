b469a59d447e5c097d26029ada3519d705981fdb
mauroindigne <mauroindigne@gmail.com>
2020-08-30 20:25:09 +0000

    removed secret key and set debug

--- one_stop_stunt_shop/settings.py
+++ one_stop_stunt_shop/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'j9=(&t2u681m+w#+s696$)+)=17kgrdwxxud$ily(s@d*=rwat'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['mauroi-one-stop-stunt-shop.herokuapp.com', 'localhost']
 
